a84f9c220dfcce0e4ad1696b08bb8f5dd41195de
x-color <36035885+x-color@users.noreply.github.com>
2020-04-28 02:14:10 +0900

    Move code of password utils

--- service/auth/auth.go
+++ service/auth/auth.go
@@ -5,12 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"time"
-	"unicode"
 
 	"github.com/x-color/calendar/model/auth"
 	cctx "github.com/x-color/calendar/model/ctx"
 	cerror "github.com/x-color/calendar/model/error"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Repogitory interface {
@@ -155,56 +153,4 @@ func (s *Service) signin(ctx context.Context, name, password string) (auth.Sessi
 	}
 
 	return session, nil
-}
-
-func validateSigninInfo(name, password string) error {
-	if name == "" {
-		return cerror.NewInvalidContentError(
-			nil,
-			"name is empty",
-		)
-	}
-	if !isValidPassword(password) {
-		return cerror.NewInvalidContentError(
-			nil,
-			"invalid password",
-		)
-	}
-	return nil
-}
-
-func isValidPassword(password string) bool {
-	hasMinLen := false
-	hasUpper := false
-	hasLower := false
-	hasNumber := false
-	hasSpecial := false
-	if 7 < len(password) && len(password) < 73 {
-		hasMinLen = true
-	}
-	for _, char := range password {
-		switch {
-		case unicode.IsUpper(char):
-			hasUpper = true
-		case unicode.IsLower(char):
-			hasLower = true
-		case unicode.IsNumber(char):
-			hasNumber = true
-		case unicode.IsPunct(char) || unicode.IsSymbol(char):
-			hasSpecial = true
-		}
-	}
-	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
-}
-
-func passwordHash(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return "", err
-	}
-	return string(hash), err
-}
-
-func verifyPassword(hash, password string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
