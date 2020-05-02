5723edfb7c907f9596849a673a101fe42dcc88fd
x-color <36035885+x-color@users.noreply.github.com>
2020-05-02 23:47:44 +0900

    Add changing calendar test

--- app/rest/server_test.go
+++ app/rest/server_test.go
@@ -465,3 +465,104 @@ func TestNewRouter_RemoveCalendar(t *testing.T) {
 		})
 	}
 }
+
+func TestNewRouter_ChangeCalendar(t *testing.T) {
+	authRepo := newAuthRepo()
+	userID, sessionID := makeSession(authRepo)
+	calRepo := newCalRepo()
+	calendarID := uuid.New().String()
+	calRepo.Calendar().Create(context.Background(), calendar.Calendar{
+		ID:      calendarID,
+		Name:    "My plans",
+		UserID:  userID,
+		Color:   calendar.RED,
+		Private: true,
+		Plans:   []calendar.Plan{},
+		Shares:  []string{userID},
+	})
+	l := newLogger()
+	as := as.NewService(authRepo, l)
+	cs := cs.NewService(calRepo, l)
+	r := newRouter(as, cs, l)
+
+	cookie := http.Cookie{
+		Name:  "session_id",
+		Value: sessionID,
+	}
+
+	testcases := []struct {
+		name   string
+		cookie *http.Cookie
+		calID  string
+		code   int
+		res    map[string]interface{}
+	}{
+		{
+			name:   "no cookie",
+			cookie: nil,
+			calID:  calendarID,
+			code:   http.StatusUnauthorized,
+			res:    map[string]interface{}{"message": "unauthorization"},
+		},
+		{
+			name:   "invalid cookie",
+			cookie: &http.Cookie{Name: "session_id", Value: uuid.New().String()},
+			calID:  calendarID,
+			code:   http.StatusUnauthorized,
+			res:    map[string]interface{}{"message": "unauthorization"},
+		},
+		{
+			name:   "invalid calendar id",
+			cookie: &cookie,
+			calID:  uuid.New().String(),
+			code:   http.StatusNotFound,
+			res:    map[string]interface{}{"message": "not found"},
+		},
+		{
+			name:   "invalid content",
+			cookie: &cookie,
+			calID:  calendarID,
+			code:   http.StatusBadRequest,
+			res:    map[string]interface{}{"message": "bad contents"},
+		},
+		{
+			name:   "not owner",
+			cookie: &cookie,
+			calID:  calendarID,
+			code:   http.StatusForbidden,
+			res:    map[string]interface{}{"message": "unauthorization"},
+		},
+		{
+			name:   "change calendar",
+			cookie: &cookie,
+			calID:  calendarID,
+			code:   http.StatusOK,
+			res:    map[string]interface{}{"message": "change calendar"},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPatch, "/calendars/"+tc.calID, nil)
+			if tc.cookie != nil {
+				req.AddCookie(tc.cookie)
+			}
+			rec := httptest.NewRecorder()
+			r.ServeHTTP(rec, req)
+
+			if rec.Code != tc.code {
+				t.Errorf("status code: want %v but %v", tc.code, rec.Code)
+			}
+
+			var actual map[string]interface{}
+			if err := json.Unmarshal(rec.Body.Bytes(), &actual); err != nil {
+				t.Errorf("invalid response body: %v", rec.Body.String())
+			}
+			expected := tc.res
+
+			if d := cmp.Diff(expected, actual, ignoreKey("id")); d != "" {
+				t.Errorf("invalid response body: \n%v", d)
+			}
+		})
+	}
+}
