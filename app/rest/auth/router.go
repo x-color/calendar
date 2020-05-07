599e29c05ec789d62ac8d8c28daa96156d4bb6b2
x-color <36035885+x-color@users.noreply.github.com>
2020-05-07 19:57:14 +0900

    Add middlewares to auth router

--- app/rest/auth/router.go
+++ app/rest/auth/router.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/x-color/calendar/app/rest/middlewares"
 	"github.com/x-color/calendar/auth/service"
 )
 
 func NewRouter(r *mux.Router, s service.Service) {
 	e := authEndpoint{s}
+	r.Use(middlewares.ReqIDMiddleware)
+	r.Use(middlewares.ResponseHeaderMiddleware)
 	r.HandleFunc("/signup", e.SignupHandler).Methods(http.MethodPost)
 	r.HandleFunc("/signin", e.SigninHandler)
-	r.HandleFunc("/signout", e.SignoutHandler)
+	r.HandleFunc("/signout", e.SignoutHandler).Methods(http.MethodPost)
 }
