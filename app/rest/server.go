93e6cbe69b8f9f225f6351772f4bfbd7b7891a66
x-color <36035885+x-color@users.noreply.github.com>
2020-06-14 02:10:11 +0900

    Add support for routing to SPA

--- app/rest/server.go
+++ app/rest/server.go
@@ -2,6 +2,8 @@ package rest
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	ase "github.com/x-color/calendar/app/rest/auth"
@@ -37,5 +39,34 @@ func newRouter(authService as.Service, calService cs.Service, l logging.Logger) 
 	pr := apiRouter.PathPrefix("/plans").Subrouter()
 	cse.NewPlanRouter(pr, calService, authService)
 
+	spa := spaHandler{staticPath: "web/calendar/dist", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
+
 	return r
+}
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
